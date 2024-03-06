d07a7f86916389dc3b1fbc63b6204fdc7fce6164
Amine lehkim. <74158555+Amine-Lehkim@users.noreply.github.com>
2024-03-07 00:58:45 +0100

    changing text color in cards

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 const NewsCard = ({ title, description, imageUrl, sourceUrl }) => (
-  <div className="overflow-hidden bg-gray-200/50 p-6 rounded-md shadow-md">
-    <img className="mb-4 rounded-md w-full h-32 object-cover no-blur" src={imageUrl} alt={title} />
-    <h2 className="text-xl font-semibold mb-2">{title}</h2>
+  <div className=" overflow-hidden bg-gray-200/60 p-6 rounded-md shadow-md">
+    {imageUrl && <img className="mb-4 rounded-md w-full h-32 object-cover no-blur" src={imageUrl} alt={title} />}
+    <h2 className="text-xl text-black font-semibold mb-2">{title}</h2>
     <p className="text-gray-600 mb-4">{description}</p>
     <a
       href={sourceUrl}
@@ -31,11 +31,13 @@ const HomePage = () => {
       .catch((error) => console.error('Error fetching news:', error));
   }, []);
 
+  const filteredNews = news.filter((newsItem) => newsItem.urlToImage);
+
   return (
     <div className="container mx-auto my-10">
       <h1 className="text-3xl font-bold mb-8">World Economy News</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {news.map((newsItem, index) => (
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 font-bold">
+        {filteredNews.map((newsItem, index) => (
           <NewsCard
             key={index}
             title={newsItem.title}
